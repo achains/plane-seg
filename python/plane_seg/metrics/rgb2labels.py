45e36a4dd57a4ae26f65c409e714e0c53f6ac9af
michael <kiselev.0353@gmail.com>
2022-07-20 11:08:12 +0300

    math fix

--- python/plane_seg/metrics/rgb2labels.py
+++ python/plane_seg/metrics/rgb2labels.py
@@ -9,5 +9,5 @@ def rgb2labels(image: Image.Image) -> NDArray[(Any, Any), Int]:
     assert (image.mode == 'RGB'), "not an RGB image"
     image_arr = np.array(image)
     reshaped = image_arr.reshape([image_arr.shape[0] * image_arr.shape[1], image_arr.shape[2]])
-    labeled = reshaped[0] * 255**2 + reshaped[1] * 255 + reshaped[2]
+    labeled = reshaped[0] * 256**2 + reshaped[1] * 256 + reshaped[2]
     return labeled
