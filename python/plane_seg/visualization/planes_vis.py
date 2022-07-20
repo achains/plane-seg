816ea07bdd1b031d3fe437acdcc5534f586106b4
Arthur_Ch <56088401+ArthurChains@users.noreply.github.com>
2022-07-21 00:12:51 +0300

    fix: visualization unique labels

--- python/plane_seg/visualization/planes_vis.py
+++ python/plane_seg/visualization/planes_vis.py
@@ -17,15 +17,14 @@ def color_planes(
     label_to_color = {0: np.zeros(shape=(1, 3))}
     color_set = {(0.0, 0.0, 0.0)}
 
-    for idx, label in enumerate(labels):
+    for label in np.unique(labels):
         if label not in label_to_color:
             col = np.random.uniform(0, 1, size=(1, 3))
             while tuple(col[0]) in color_set:
                 col = np.random.uniform(0, 1, size=(1, 3))
             label_to_color[label] = col
             color_set.add(tuple(col[0]))
-
-        colors[idx] = label_to_color[label]
+        colors[labels == label] = label_to_color[label]
 
     colored_pcd.colors = o3d.utility.Vector3dVector(colors)
 
