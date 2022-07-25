674e263fdbb9abea3a3bdc4d34192f409481b724
true-real-michael <64229743+true-real-michael@users.noreply.github.com>
2022-07-25 14:50:54 +0300

    Added MultiValueBenchmark with threshold to metrics (#8)
    * added multivalue with threshold
    
    * comments
    
    * black stylefix
    
    * clearer code + added multivalue to examples
    
    * black stylefix
    
    * otuput format correction
    
    * fixed threshold usage
    
    * split instead of slicing

--- python/plane_seg/metrics/metrics_evaluation.py
+++ python/plane_seg/metrics/metrics_evaluation.py
@@ -19,6 +19,7 @@ def evaluate_metrics(
         "fScore-iou",
         "mean-iou",
         "mean-dice",
+        "multivalue-0.8",  # Here user can specify any float between 0 and 1 as a threshold
     ),
     print_to_console: bool = True,
     output_file: Path = None,
@@ -28,7 +29,7 @@ def evaluate_metrics(
 
     :param prediction: an RGB image or a 2D NumPy array
     :param ground_truth_path: path to an RGB image or a 2D NumPy array, the same size as predictions
-    :param metric_names: names of EVOPS metrics: any subset of: {iou, dice, precision-iou, recall-iou, fScore-iou, mean-iou, mean-dice}. All by default
+    :param metric_names: names of EVOPS metrics: any subset of: {iou, dice, precision-iou, recall-iou, fScore-iou, mean-iou, mean-dice, multivalue-0.X}. All by default
     :param print_to_console: prints to console if true
     :param output_file: appends output to the file if specified
     :return: the value of the metric
@@ -46,30 +47,44 @@ def evaluate_metrics(
     metric_values = {}
 
     for metric_name in metric_names:
-        if "iou" == metric_name:
-            metric_values["iou"] = iou(prediction, ground_truth)
-        elif "dice" == metric_name:
-            metric_values["dice"] = dice(prediction, ground_truth)
-        elif "precision-iou" == metric_name:
-            metric_values["precision-iou"] = precision(prediction, ground_truth, "iou")
-        elif "recall-iou" == metric_name:
-            metric_values["recall-iou"] = recall(prediction, ground_truth, "iou")
-        elif "fScore-iou" == metric_name:
-            metric_values["fScore-iou"] = fScore(prediction, ground_truth, "iou")
-        elif "mean-iou" == metric_name:
-            metric_values["mean-iou"] = mean(prediction, ground_truth, iou)
-        elif "mean-dice" == metric_name:
-            metric_values["mean-dice"] = mean(prediction, ground_truth, dice)
+        if metric_name == "iou":
+            metric_values[metric_name] = iou(prediction, ground_truth)
+        elif metric_name == "dice":
+            metric_values[metric_name] = dice(prediction, ground_truth)
+        elif metric_name == "precision-iou":
+            metric_values[metric_name] = precision(prediction, ground_truth, "iou")
+        elif metric_name == "recall-iou":
+            metric_values[metric_name] = recall(prediction, ground_truth, "iou")
+        elif metric_name == "fScore-iou":
+            metric_values[metric_name] = fScore(prediction, ground_truth, "iou")
+        elif metric_name == "mean-iou":
+            metric_values[metric_name] = mean(prediction, ground_truth, iou)
+        elif metric_name == "mean-dice":
+            metric_values[metric_name] = mean(prediction, ground_truth, dice)
+        elif metric_name.startswith("multivalue"):
+            splitted = metric_name.split("-")
+            if len(splitted) == 2:
+                threshold = np.float64(splitted[1])
+                if not (0 <= threshold <= 1):
+                    raise ValueError(f"Invalid multivalue threshold")
+            else:
+                threshold = np.float64(0.8)
+            multivalue_result = multi_value(prediction, ground_truth, threshold)
+
+            metric_values["under_segmented"] = multivalue_result["under_segmented"]
+            metric_values["over_segmented"] = multivalue_result["over_segmented"]
+            metric_values["missed"] = multivalue_result["missed"]
+            metric_values["noise"] = multivalue_result["noise"]
         else:
             raise ValueError(f"Invalid metric name {metric_name}.")
 
     if print_to_console:
         for metric, value in metric_values.items():
-            print(f"{metric:13} {value}")
+            print(f"{metric:15} {value}")
     if output_file is not None:
         with open(output_file, "w") as fout:
             for metric, value in metric_values.items():
-                fout.write(f"{metric:13} {value}\n")
+                fout.write(f"{metric:15} {value}\n")
             fout.write("\n")
 
     return metric_values
